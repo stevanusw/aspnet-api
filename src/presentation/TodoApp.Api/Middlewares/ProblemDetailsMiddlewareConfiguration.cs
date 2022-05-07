f9850c414678b9f9ada9fb99e4186c8eef0234cb
stevanusw <wibowo@hotmail.com>
2022-05-07 18:25:45 +1000

    Log unhandled exception.

--- src/presentation/TodoApp.Api/Middlewares/ProblemDetailsMiddlewareConfiguration.cs
+++ src/presentation/TodoApp.Api/Middlewares/ProblemDetailsMiddlewareConfiguration.cs
@@ -1,7 +1,6 @@
 ﻿using Hellang.Middleware.ProblemDetails;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TodoApp.Models.Exceptions;
